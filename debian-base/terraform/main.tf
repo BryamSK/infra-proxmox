c9cda7a5bb9ea66868833bfbef2ce51986264916
BryamSK <bryam.hf@gmail.com>
2025-08-12 14:40:15 -0500

    feat: add Proxmox VM configuration and provider setup for improved deployment

--- debian-base/terraform/main.tf
+++ debian-base/terraform/main.tf
@@ -1,58 +1,8 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "3.0.2-rc03"
-    }
-  }
-}
-
-variable "proxmox_api_url" {
-  type      = string
-  sensitive = true
-}
-variable "proxmox_username" {
-  type      = string
-  sensitive = true
-}
-variable "proxmox_token_id" {
-  type      = string
-  sensitive = true
-}
-variable "proxmox_token" {
-  type      = string
-  sensitive = true
-}
-variable "storage_pool" {
-  description = "Storage pool for VM disks"
-  type        = string
-}
-variable "vm_user" {
-  description = "SSH user for cloned template"
-  type        = string
-}
-variable "vm_private_key_path" {
-  description = "Path to private SSH key for provisioning"
-  type        = string
-}
-variable "node" {
-  description = "Proxmox node where VMs will be created"
-  type        = string
-}
-
-provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_user             = var.proxmox_username
-  pm_api_token_id     = var.proxmox_token_id
-  pm_api_token_secret = var.proxmox_token
-  pm_tls_insecure     = true
-}
-
-resource "proxmox_vm_qemu" "debian12" {
+resource "proxmox_vm_qemu" "debian" {
   count           = 1
-  name            = "debian12-${count.index + 1}"
+  name            = "${var.template}-${count.index + 1}"
   target_node     = var.node
-  clone           = "debian12"
+  clone           = var.template
   full_clone      = true
   bootdisk        = "scsi0"
   scsihw          = "virtio-scsi-pci"
@@ -95,5 +45,5 @@ resource "proxmox_vm_qemu" "debian12" {
 }
 
 output "vm_ips" {
-  value = [for vm in proxmox_vm_qemu.debian12 : vm.ssh_host]
+  value = [for vm in proxmox_vm_qemu.debian : vm.ssh_host]
 }
---
+++ debian-base/terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "proxmox_api_url" {
+  type      = string
+  sensitive = true
+}
+variable "proxmox_username" {
+  type      = string
+  sensitive = true
+}
+variable "proxmox_token_id" {
+  type      = string
+  sensitive = true
+}
+variable "proxmox_token" {
+  type      = string
+  sensitive = true
+}
+variable "storage_pool" {
+  description = "Storage pool for VM disks"
+  type        = string
+}
+variable "vm_user" {
+  description = "SSH user for cloned template"
+  type        = string
+}
+variable "vm_private_key_path" {
+  description = "Path to private SSH key for provisioning"
+  type        = string
+}
+variable "node" {
+  description = "Proxmox node where VMs will be created"
+  type        = string
+}
+variable "template" {
+  description = "Name of the template to clone"
+  type        = string
+}
